795b71f383e27c20fd514abc94a8ec20713725ae
Kabera Nshuti Samuel <167257474+KaberaSamuel@users.noreply.github.com>
2024-10-15 22:59:29 +0200

    Add files via upload
    creating a fetch query to get weather data

---
+++ main.js
@@ -0,0 +1,23 @@
+async function logWeather(city) {
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&key=4GGPTYXEW5G2EF9FQJUVV9KX3&contentType=json`;
+
+  let response = await fetch(url, { mode: "cors" });
+  response = await response.json();
+  console.log(response);
+  console.log("");
+  console.log("");
+  response = response.currentConditions;
+
+  const temperature = ((response.temp - 32) * 5) / 9;
+
+  const conditions = response.conditions;
+  const humidity = response.humidity;
+  const windspeed = response.windspeed;
+
+  console.log("temp: " + temperature);
+  console.log("conditions: " + conditions);
+  console.log("humidity: " + humidity);
+  console.log("windspeed: " + windspeed);
+}
+
+const result = logWeather("kigali");
