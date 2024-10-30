6c08e0bd74c397a9e0899f2497e87cfad61ea5a5
KaberaSamuel <kaberanshutis@gmail.com>
2024-10-30 23:18:08 +0200

    creating map to show searched cities

--- script/map.js
+++ script/map.js
@@ -1,1 +1,12 @@
 displayCities();
+
+function showMap() {
+  const map = L.map("map").setView([0, 0], 13);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+}
+
+showMap();
